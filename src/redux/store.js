f304bf146145ba22e08e384a479d7df4e057d2b9
Mkqupy <121451774+Mkqupy@users.noreply.github.com>
2025-04-10 18:07:06 +0300

    save redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,6 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
+import contactReducer from './reducers';
 
-const initialState = {
-  counter: 0,
-};
+const store = createStore(contactReducer);
 
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case "plus":
-      return {
-        counter: state.counter + 1,
-      };
-
-    case "minus":
-      return {
-        counter: state.counter - 1,
-      };
-
-    default:
-      return state;
-  }
-};
-
-// const rootReducer = (state = initialState, action) => {
-//   return state;
-// };
-
-export const store = createStore(reducer, initialState);
-
-// {...oldState, ...newState} => {...{counter: 0}, ...{counter: 2}} => отримуємо {counter: 0, counter: 2} => {counter: 2}
+export default store;
